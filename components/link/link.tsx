6c1123079b37a107fc0b932607a415165418da31
jnoodle <fullstack@126.com>
2020-07-28 17:16:08 +0800

    feat(link): adjust props according to design
    add `plain`, `iconLeft`, `iconRight`, `disabled` props, deprecate `color`, `icon` props

--- components/link/link.tsx
+++ components/link/link.tsx
@@ -7,20 +7,26 @@ import LinkIcon from './icon'
 export interface Props {
   href?: string
   color?: boolean
+  plain?: boolean
   pure?: boolean
   icon?: boolean
+  iconLeft?: React.ReactNode
+  iconRight?: React.ReactNode
   underline?: boolean
   block?: boolean
+  disabled?: boolean
   className?: string
 }
 
 const defaultProps = {
   href: '',
   color: false,
+  plain: false,
   pure: false,
   icon: false,
   underline: false,
   block: false,
+  disabled: false,
   className: '',
 }
 
@@ -29,22 +35,52 @@ export type LinkProps = Props & typeof defaultProps & NativeAttrs
 
 const Link = React.forwardRef<HTMLAnchorElement, React.PropsWithChildren<LinkProps>>(
   (
-    { href, color, underline, pure, children, className, block, icon, ...props },
+    {
+      href,
+      color,
+      plain,
+      pure,
+      icon,
+      iconLeft,
+      iconRight,
+      underline,
+      block,
+      disabled,
+      className,
+      children,
+      ...props
+    },
     ref: React.Ref<HTMLAnchorElement>,
   ) => {
     const theme = useTheme()
-    const linkColor = color || block ? theme.palette.link : 'inherit'
-    const hoverColor = color || block ? theme.palette.successLight : 'inherit'
-    const padding = block ? theme.layout.gapQuarter : '0'
+    const linkColor = plain ? 'inherit' : theme.palette.cTheme5
+    const hoverColor = theme.palette.cTheme6
+    const disabledColor = theme.palette.cGray5
+    const padding = block ? '2px 4px' : '0'
     const decoration = underline ? 'underline' : 'none'
     if (pure) {
-      useWarning('Props "pure" is deprecated, now the default Link is pure.')
+      useWarning('Props "pure" is deprecated, now the default Link is pure.', 'Link')
+    }
+    if (color) {
+      useWarning('Props "color" is deprecated, now the default Link is colored.', 'Link', 'info')
+    }
+    if (icon) {
+      useWarning(
+        'Props "icon" is no longer supported, use "iconLeft" or "iconRight" instead.',
+        'Link',
+        'info',
+      )
     }
 
     return (
-      <a className={`link ${className}`} href={href} {...props} ref={ref}>
+      <a
+        href={href}
+        className={`link ${className}${disabled ? ' disabled' : ''}`}
+        ref={ref}
+        {...props}>
+        {iconLeft && <LinkIcon icon={iconLeft} className="left" />}
         {children}
-        {icon && <LinkIcon />}
+        {iconRight && <LinkIcon icon={iconRight} className="right" />}
         <style jsx>{`
           .link {
             display: inline-flex;
@@ -52,7 +88,7 @@ const Link = React.forwardRef<HTMLAnchorElement, React.PropsWithChildren<LinkPro
             line-height: inherit;
             color: ${linkColor};
             text-decoration: none;
-            padding: calc(${padding} * 0.8) calc(${padding} * 1.7);
+            padding: ${padding};
             border-radius: ${block ? theme.expressiveness.R2 : 0};
             width: fit-content;
             transition: color 200ms ease 0ms;
@@ -67,6 +103,20 @@ const Link = React.forwardRef<HTMLAnchorElement, React.PropsWithChildren<LinkPro
           .link:hover {
             background-color: ${block ? '#0076ff1a' : 'unset'};
             color: ${hoverColor};
+          }
+
+          .link.disabled {
+            color: ${disabledColor};
+            pointer-events: none;
+            cursor: default;
+          }
+
+          .link :global(.left) {
+            margin-left: 0;
+          }
+
+          .link :global(.right) {
+            margin-right: 0;
           }
         `}</style>
       </a>
